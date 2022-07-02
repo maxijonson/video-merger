385cf9c6dba1cc27b030af8de9e6b8f577f6e1c7
Tristan Chin <tristan.chin@chintristan.io>
2022-07-02 01:09:19 -0400

    Fix video merging

--- src/utils/createFfmpegCommand.ts
+++ src/utils/createFfmpegCommand.ts
@@ -1,6 +1,9 @@
 const createFfmpegCommand = (filePaths: string[], outputPath: string) => {
-    const input = filePaths.map((f) => f.replace(/\\/g, "/")).join("|");
-    return `ffmpeg -i "concat:${input}" -c copy ${outputPath}`;
+    const input = filePaths
+        .map((f) => `-i "${f.replace(/\\/g, "/")}"`)
+        .join(" ");
+    const n = filePaths.length;
+    return `ffmpeg ${input} -filter_complex "concat=n=${n}:v=1:a=1" -y ${outputPath}`;
 };
 
 export default createFfmpegCommand;
